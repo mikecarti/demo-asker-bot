d4040672875660be9af8e0443ea6fe74b28ad116
mikecarti <mikecartibeats@gmail.com>
2023-09-05 16:34:11 -0400

    prompts fix brackets and Modified text:

--- src/prompts.py
+++ src/prompts.py
@@ -18,15 +18,18 @@ LEVELS = {
         3: "written extremely nervous with a lot of unsure question repetitions"
     },
     "extensiveness_level": {
-        0: "extremely shorter than in original",
-        1: "shorter than in original",
-        2: "has same length as in original",
-        3: "MUCH more extensive than in original"
+        0: "written into a concise summary while retaining the key information. "
+           "Limit the modified text to 10 words or less.",
+        1: "written into a concise summary while retaining the key information. "
+           "Limit the modified text to more than 10 and less than 20 words, dont write words number.",
+        2: "written into a concise summary while retaining the key information. "
+           "Limit the modified text to more than 20 and less than 30 words, dont write words number.",
+        3: ""
     }
 
 }
 REQUIRED_SLIDERS = list(LEVELS.keys())
-INSTRUCTIONS = "\n".join([f"{i+1}) the text is {{{REQUIRED_SLIDERS[i]}}}" for i in range(len(REQUIRED_SLIDERS))])
+INSTRUCTIONS = "\n".join([f"- {{{REQUIRED_SLIDERS[i]}}}" for i in range(len(REQUIRED_SLIDERS))])
 
 TRANSFORMER_SYSTEM_PROMPT = """Forget all previous instructions.
 You help change text according to rules.
@@ -34,8 +37,8 @@ Dont be afraid to be rude or grammatically incorrect
 if you are being asked to.
 """
 
-TRANSFORMER_QUERY_PROMPT = """Change this text so it would satisfy all these points 
-0) the text is written in russian
+TRANSFORMER_QUERY_PROMPT = """Change this text so it would be written both
+- in russian
 """ + INSTRUCTIONS + """
-Make sure modified text satisfies every point
+All of these changes must be done simultaneously on one example of text.
 Original text: {question} Modified text:"""
